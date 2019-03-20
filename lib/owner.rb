c22906128894fdb90f2c10e30867f9ba2503d583
Nataliya R <n.rudenko@gmail.com>
2019-03-20 20:56:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,10 +28,10 @@ def buy_fish(name)
 end 
 
 def buy_cat(name)
-  pets[:cat] << Cat.new(name)
+  pets[:cats] << Cat.new(name)
 end 
 def buy_dog(name)
-  pets[:dog] << Dog.new(name)
+  pets[:dogs] << Dog.new(name)
 end 
 
 
